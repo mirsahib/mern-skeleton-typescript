f88372ec3075fde31b27dafdb0f2004675acc1a0
mirhabibul.latif@techhack.ca <mirsahib24@gmail.com>
2021-10-04 15:29:22 +0600

    :fire: Delete config.js

--- config/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    mongoUri: process.env.MONGODB_URI ||
-    process.env.MONGO_HOST ||
-    'mongodb://' + (process.env.IP || 'localhost') + ':' +
-    (process.env.MONGO_PORT || '27017') +
-    '/mernproject'
-}
-
-export default config
