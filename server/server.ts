f39009008db559f8ea95c748cedbc01b2e937fde
mirhabibul.latif@techhack.ca <mirsahib24@gmail.com>
2021-10-04 15:22:56 +0600

    :hammer: Add mongoose db connection config

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,13 @@
 import app from './express'
+import config from '../config/config'
 import mongoose, { mongo } from 'mongoose'
 
 //Connection URL
-
-// mongoose.Promise = global.Promise
-// mongoose.connect('')
-// mongoose.connection.on('error',()=>{
-//     throw new Error(`unable to connect to database`)
-// })
+mongoose.Promise = global.Promise
+mongoose.connect(config.mongoUri)
+mongoose.connection.on('error',()=>{
+   throw new Error(`unable to connect to database`)
+})
 
 app.listen(5000,()=>{
     console.log('Server started on port %s.',5000)
