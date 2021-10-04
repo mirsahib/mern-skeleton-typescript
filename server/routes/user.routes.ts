807e31f262d4e6dab98b67d675cf8bfbde2f6631
mirhabibul.latif@techhack.ca <mirsahib24@gmail.com>
2021-10-04 15:24:54 +0600

    :hammer: Add user create route

--- server/routes/user.routes.ts
+++ server/routes/user.routes.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import userCtrl from '../controllers/user.controller'
 const router = express.Router()
 
 
 router.route('/api/users')
+.post(userCtrl.create)
 
 router.route('/api/users/:userId')
 
